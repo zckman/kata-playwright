1c2727bcad4703075f9bf6f5e20456a4a924c4ab
Dennis Syhre <dennis.syhre@bergfreunde.de>
2024-03-22 11:56:11 +0100

    Add solution for 01

--- tests/01.spec.ts
+++ tests/01.spec.ts
@@ -1,1 +1,17 @@
 // @ts-check
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByText('Wonach suchst Du?').click();
+  await page.getByPlaceholder('Wonach suchst Du?').fill('Wanderschuhe');
+  await page.getByPlaceholder('Wonach suchst Du?').press('Enter');
+  await page.getByRole('link', { name: 'Stoic - SälkaSt. Hiking Shoes - Wanderschuhe 15% Stoic SälkaSt. Hiking Shoes Wanderschuhe € 119,95* €' }).click();
+  await page.getByRole('link', { name: 'Carbon' }).click();
+  await page.getByRole('link', { name: '41' }).click();
+  await page.getByRole('button', { name: 'in den Warenkorb' }).click();
+  await page.getByRole('button', { name: 'zurück zum Produkt' }).click();
+  await expect(page.getByTestId('miniBasket')).toHaveText("1")
+  await page.goto('/warenkorb/')
+  await expect(page.getByTestId('cartItemAmount')).toHaveValue("1")
+});

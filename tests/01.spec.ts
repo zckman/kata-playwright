c57d6abae6a572bde13317077f2dad745c225fdc
Dennis <dennis.syhre@bergfreunde.de>
2024-04-08 09:09:42 +0200

    Add some comments to 01.spec.ts

--- tests/01.spec.ts
+++ tests/01.spec.ts
@@ -3,9 +3,12 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('/');
+  // search input
   await page.getByText('Wonach suchst Du?').click();
+  // this is a different input element with a placeholder
   await page.getByPlaceholder('Wonach suchst Du?').fill('Wanderschuhe');
   await page.getByPlaceholder('Wonach suchst Du?').press('Enter');
+  // this can break the test if the specific product is missing
   await page.getByRole('link', { name: 'Stoic - SälkaSt. Hiking Shoes - Wanderschuhe 15% Stoic SälkaSt. Hiking Shoes Wanderschuhe € 119,95* €' }).click();
   await page.getByRole('link', { name: 'Carbon' }).click();
   await page.getByRole('link', { name: '41' }).click();
